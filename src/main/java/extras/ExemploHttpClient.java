5fc8c6134de787f2dc38e5073c8c189d3024a2df
c-ferraz <cs.ferraz@outlook.com>
2021-03-14 19:07:58 -0300

    exemplo basico usando HttpClient

---
+++ src/main/java/extras/ExemploHttpClient.java
@@ -0,0 +1,30 @@
+package extras;
+
+import com.sun.net.httpserver.Headers;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ExemploHttpClient {
+    public static void main(String[] args) {
+
+    }
+
+    private static void connectAndPrintURLJavaOracle() throws IOException, InterruptedException{
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET().uri(URI.create("https://docs.oracle.com/javase/10/language/"))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status code :: " + response.statusCode());
+        System.out.println("Headers response :: " + response.headers());
+//        System.out.println(response.body());
+    }
+
+}
