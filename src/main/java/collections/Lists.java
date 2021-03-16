ec5d9b895b1a003d148ff08d723c69006d2ebf26
c-ferraz <cs.ferraz@outlook.com>
2021-03-16 17:14:09 -0300

    Exemplo instanciar lista

--- src/main/java/collections/Lists.java
+++ src/main/java/collections/Lists.java
@@ -1,7 +1,10 @@
 package collections;
 
+import java.util.List;
+
 public class Lists {
     public static void main(String[] args) {
+//        List<String> nomes = List.of("João", "Maria", "José"); // Maneira mais facil de instanciar listas
 
     }
 }
